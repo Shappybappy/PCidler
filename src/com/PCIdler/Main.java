fc0c480d2794f0c7ac16b281d2cd05682eea2413
Shapsty <shapsty@gmail.com>
2022-05-02 17:27:00 -0500

    collapsed list of coords into a array

--- src/com/PCIdler/Main.java
+++ src/com/PCIdler/Main.java
@@ -7,30 +7,23 @@ public class Main {
     public static void main(String[] args) {
         //run in command prompt lol
         while (true){
-            int coordX1 = 120;
-            int coordY1 = 150;
-            int coordX2 = 100;
-            int coordY2 = 100;
-            int coordX3 = 230;
-            int coordY3 = 210;
-
             try {
                 Robot robby = new Robot();
-                //moves your cursor every quarter second 11 times
-                for (int j = 0; j < 16; j++) {
+                int[] arr = new int[]{15, 20, 45, 30, 50};
+                //moves your cursor every quarter second 20 times
+                for (int j = 0; j < 21; j++) {
                     System.out.println("J: " + j);
-                    robby.mouseMove(coordX1, coordY1);
+                    robby.mouseMove(arr[0], arr[1]);
                     TimeUnit.MILLISECONDS.sleep(250);
-                    robby.mouseMove(coordX2, coordY2);
+                    robby.mouseMove(arr[2], arr[3]);
                     TimeUnit.MILLISECONDS.sleep(250);
-                    robby.mouseMove(coordX3, coordY3);
+                    robby.mouseMove(arr[3], arr[4]);
                     TimeUnit.MILLISECONDS.sleep(250);
                 }
-            } catch (AWTException e) {
+            } catch (AWTException | InterruptedException e) {
                 e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } for (int i = 0; i < 180; i++){
+            }
+            for (int i = 0; i < 180; i++){
                 //Every three minutes this loop quits going back up to the cursor loop
                 System.out.println("I: " + i);
                 try {
